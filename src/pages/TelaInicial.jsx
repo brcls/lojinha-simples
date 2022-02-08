5b0e01252f61f0180b5958e6ed0caaa707db21e1
brcls <petrick_barcelos@hotmail.com>
2022-02-08 17:49:00 -0300

    melhorando tela de cadastro e Login

--- src/pages/TelaInicial.jsx
+++ src/pages/TelaInicial.jsx
@@ -1,14 +1,28 @@
 import React from "react";
-import { Button, Container } from '@mui/material';
+import { Button, Grid, Container } from '@mui/material';
 import LojaLogo from '../assets/loja.png';
+import {Link} from 'react-router-dom';
 
 function TelaIncial(){
     return(
-        <Container maxWidth="sm">
+        <Grid
+        container
+        spacing={12}
+        sx={{mt: 17}}
+        direction="column"
+        justifyContent="space-around"
+        alignItems="center"
+        >
             <img src={LojaLogo} alt="loja logo" width="150" height="150"/>
-            <Button variant="contained" fullWidth >Cadastro</Button>
-            <Button variant="contained" fullWidth>Login</Button>
-        </Container>
+            <Container  maxWidth="xs" sx={{mt:"3%"}}>
+                <Link to="/cadastro" style={{ textDecoration: 'none' }}>
+                    <Button fullWidth sx={{margin: 1, background: "#50c878", color: "white", fontWeight: "bold", boxShadow: 5}}>Cadastro</Button>
+                </Link>
+                <Link to="/login" style={{ textDecoration: 'none' }}>
+                    <Button fullWidth sx={{margin: 1, background: "#50c878", color: "white", fontWeight: "bold", boxShadow: 5}}>Login</Button>
+                </Link>
+            </Container>
+        </Grid>
     )
 }
 
